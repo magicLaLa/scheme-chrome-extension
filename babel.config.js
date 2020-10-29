8d827a0bd73c4461cc77899ef67f6275fe84cb85
yichen <tomato.stao@gmail.com>
2020-10-29 23:16:47 +0800

    :ambulance: 更新config配置和初步搭建chrome插件开发环境

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "import",
+      {
+          "libraryName": "view-design",
+          "libraryDirectory": "src/components"
+      }
+    ],
+    "@babel/plugin-proposal-optional-chaining",
+  ]
 };
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,54 @@
-const fs = require('fs');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ZipWebpackPlugin = require('zip-webpack-plugin');
 
-const dirs = fs.readdirSync(__dirname + '/src/views', { encoding: 'utf-8' });
+// 只需要复制的文件
+const copyFiles = [
+  {
+    from: path.resolve('src/manifest.json'),
+    to: `${path.resolve('dist')}/manifest.json`
+  },
+  {
+    from: path.resolve('src/assets'),
+    to: path.resolve('dist/assets')
+  },
+];
 
+const chromeNames = [
+  'popup',
+];
 const pages = {};
-dirs.map(dir => {
-  const name = dir.toLowerCase();
+
+if (process.env.NODE_ENV !== 'production') {
+  copyFiles.push({
+    from: path.resolve('src/utils/hot-reload.js'),
+    to: path.resolve('dist')
+  });
+}
+
+chromeNames.map(name => {
   pages[name] = {
-    entry: `./src/views/${dir}/main.ts`,
+    entry: `./src/${name}/main.ts`,
     template: 'index.html',
     filename: `${name}.html`,
-    title: `${name.toUpperCase()}`
+    title: `${name}`
   };
 });
 
-console.log(pages);
+const plugins = [
+  new CopyWebpackPlugin({
+    patterns: copyFiles,
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new ZipWebpackPlugin({
+      path: path.resolve('dist'),
+      filename: 'dist.zip'
+    })
+  );
+}
 
 module.exports = {
   pages,
@@ -22,24 +56,29 @@ module.exports = {
   productionSourceMap: false,
   css: {
     extract: {
-      filename: "css/[name].css"
+      filename: 'css/[name].css'
+    }
+  },
+  configureWebpack: {
+    plugins,
+    output: {
+      filename: 'js/[name].js'
     }
   },
   chainWebpack: config => {
-    config.resolve.alias.set("@", path.resolve("src"));
+    config.resolve.alias
+      .set('@', path.resolve('src'));
     // 处理字体文件名，去除hash值
     const fontsRule = config.module.rule("fonts");
-
     // 清除已有的所有 loader。
     // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
     fontsRule.uses.clear();
-    fontsRule
-      .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
-      .use("url")
-      .loader("url-loader")
+    fontsRule.test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
+      .use('url')
+      .loader('url-loader')
       .options({
         limit: 1000,
-        name: "fonts/[name].[ext]"
+        name: 'fonts/[name].[ext]'
       });
   },
 };
